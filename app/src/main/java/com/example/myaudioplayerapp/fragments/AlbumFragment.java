e641f6f7eb8b92d9c9addbedbcff4eba5a9dcdfe
harsh gandhi <harshgandhi043@gmail.com>
2022-05-15 15:21:36 +0530

    Made main screen:  - Made tab layout for songs and albums  - Added storage permission and made function for requesting the user for the same

---
+++ app/src/main/java/com/example/myaudioplayerapp/fragments/AlbumFragment.java
@@ -0,0 +1,41 @@
+package com.example.myaudioplayerapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myaudioplayerapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AlbumFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AlbumFragment extends Fragment {
+
+
+    public AlbumFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static AlbumFragment newInstance(String param1, String param2) {
+        return new AlbumFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_album, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/myaudioplayerapp/fragments/SongFragment.java
@@ -0,0 +1,40 @@
+package com.example.myaudioplayerapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myaudioplayerapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SongFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SongFragment extends Fragment {
+
+
+    public SongFragment() {
+        // Required empty public constructor
+    }
+
+    public static SongFragment newInstance() {
+        return new SongFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_song, container, false);
+    }
+}
