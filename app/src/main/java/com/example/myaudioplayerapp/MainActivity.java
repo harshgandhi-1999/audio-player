257dff1a0d8a9680d48fe5abd08b6a785e9339fc
harsh gandhi <harshgandhi043@gmail.com>
2022-05-15 15:59:05 +0530

    made a fucntion for fetching all audio files from storage

--- app/src/main/java/com/example/myaudioplayerapp/MainActivity.java
+++ app/src/main/java/com/example/myaudioplayerapp/MainActivity.java
@@ -7,13 +7,20 @@ import androidx.core.content.ContextCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.myaudioplayerapp.adapters.ViewPagerAdapter;
 import com.example.myaudioplayerapp.fragments.AlbumFragment;
 import com.example.myaudioplayerapp.fragments.SongFragment;
+import com.example.myaudioplayerapp.models.MusicFile;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
@@ -21,12 +28,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     public static final int REQUEST_CODE = 1;
     private TabLayout tabLayout;
     private ViewPager2 viewPager;
     private ViewPagerAdapter adapter;
     private final ArrayList<String> tabTitles = new ArrayList<String>(Arrays.asList("Songs","Albums"));
+    private ArrayList<MusicFile> musicFiles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
         if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE);
+        }else{
+            musicFiles = getAllAudio(this);
         }
     }
 
@@ -68,9 +79,43 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode==REQUEST_CODE){
             if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
                 Toast.makeText(this, "Permission granted 1", Toast.LENGTH_SHORT).show();
+                musicFiles = getAllAudio(this);
             }else if (grantResults[0]==PackageManager.PERMISSION_DENIED){
                 ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE);
             }
         }
     }
+
+    public static ArrayList<MusicFile> getAllAudio(Context context){
+        ArrayList<MusicFile> audioList = new ArrayList<>();
+
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = {
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.ALBUM,
+                MediaStore.Audio.Media.DURATION,
+        };
+
+        Cursor cursor = context.getContentResolver().query(uri,projection,null,null,null);
+
+        if(cursor!=null){
+            while (cursor.moveToNext()){
+                String path = cursor.getString(0);
+                String title = cursor.getString(1);
+                String artist = cursor.getString(2);
+                String album = cursor.getString(3);
+                String duration = cursor.getString(4);
+
+                MusicFile musicFile = new MusicFile(path,title,artist,album,duration);
+                Log.i(TAG, "getAllAudio: " + " TITLE = " + musicFile.getTitle() );
+                audioList.add(musicFile);
+            }
+            cursor.close();
+        }
+
+        return audioList;
+    }
+
 }
